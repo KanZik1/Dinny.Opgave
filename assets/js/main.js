a135de69106e12129c5f53692267911099a6e673
KanZik1 <christensson@hotmail.dk>
2023-12-12 09:14:54 +0100

    Fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,27 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Get the modal
+    // Få modalen
     var modal = document.getElementById("signinModal");
 
-    // Get the button that opens the modal
+    // Få knappen som åbner modalen
     var btn = document.querySelector("nav ul:nth-child(2) li a");
 
 
-    // Get the element that closes the modal
+    // Få elementet der lukker modalen
     var span = document.getElementsByClassName("close-button")[0];
 
 
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
+    // Når brugeren trykker på knappen åbner modalen
+    btn.onclick = function () {
         modal.style.display = "block";
     }
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
+    // Når brugeren trykker på X lukker modalen
+    span.onclick = function () {
         modal.style.display = "none";
     }
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
+    // Hvis bruger trykker uden for modal vinduet lukker det
+    window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
@@ -31,28 +31,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 });
 
-document.getElementById('signinForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevents the default form submission
-  
+document.getElementById('signinForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
     var firstName = document.getElementById('firstName').value;
     var secondName = document.getElementById('secondName').value;
     var email = document.getElementById('email').value;
     var phone = document.getElementById('phone').value;
     var date = document.getElementById('date').value;
-  
 
-    //simple validation checks
+
+    //Validation check
     if (!firstName || !secondName || !email || !phone || !date) {
         alert('Please fill out all fields.');
-        return; // Stops the function if any field is empty
+        return; // Stopper funktionen hvis nogen felter er tomme
     }
-    // Display the validation message
+    // Visning af validation beskeden
     var validationMessage = document.getElementById('validationMessage');
     validationMessage.textContent = 'Thank you for reserving a table';
     validationMessage.style.display = 'block';
 
 
     console.log('First Name:', firstName, 'Second Name:', secondName, 'Email:', email, 'Phone:', phone, 'Date:', date);
-  
-    // Here you can add code to handle the form data, like sending it to a server
-  });
+
+
+});
